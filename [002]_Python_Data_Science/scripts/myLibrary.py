01035cc24e81699c0c0210cdf80230179bcdbe0e
takytaky <rusita@empal.com>
2020-08-12 21:01:00 +0900

    20200812
    KMAC

---
+++ [002]_Python_Data_Science/scripts/myLibrary.py
@@ -0,0 +1,6 @@
+﻿def hi():
+    print('첫번째 함수 실행')
+
+def hap(x, y):
+    z = x + y
+    return z
---
+++ pyData/hello.py
@@ -0,0 +1,4 @@
+﻿def hi():
+    print('사용자 정의 함수 실행')
+    print('Hello World')
+    print('사용자 정의 함수 종료')
